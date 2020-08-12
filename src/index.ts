c496e0b8963f383bcdfa9a06792e5bda853be1a1
skipperod <mr.fawadahmed91@gmail.com>
2020-08-12 08:12:08 +0500

    initial project directory setup

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import "reflect-metadata";
+import { ApolloServer } from "apollo-server-express";
+import Express from "express";
+import { buildSchema } from "type-graphql";
+import { createConnection } from "typeorm";
+
+
+
+
+
+
+
+const main = async () => {
+    
+  };
+
+
+  main().catch(console.error);
